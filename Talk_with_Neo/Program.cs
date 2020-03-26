0b110a716d9967327a23aceabeba8c971a29611e
NazTanya <tiger04298@gmail.com>
2020-03-26 21:28:00 +0700

    Добавьте файлы проекта.

---
+++ Talk_with_Neo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Talk_with_Neo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            List<string> str = new List<string>{"Wake up Neo...", "The Matrix has you...",
+                                                "Follow the white rabbite...", "Knoc, knoc Neo..." };
+            for (int i = 0; i < str.Count; i++) 
+                Write_to_Console(str[i]); 
+            
+        }
+
+        public static  void Write_to_Console(string a)
+        {
+            Console.WriteLine(a);
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
