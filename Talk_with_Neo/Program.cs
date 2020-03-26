b82112474064e17f07e05222f44922da2fbaa3e1
NazTanya <tiger04298@gmail.com>
2020-03-27 00:16:31 +0700

    sleep

--- Talk_with_Neo/Program.cs
+++ Talk_with_Neo/Program.cs
@@ -11,14 +11,13 @@ namespace Talk_with_Neo
             Console.ForegroundColor = ConsoleColor.Green;
             List<string> str = new List<string>{"Wake up Neo...", "The Matrix has you...",
                                                 "Follow the white rabbite...", "Knoc, knoc Neo..." };
-            for (int i = 0; i < str.Count; i++) 
-                Write_to_Console(str[i]); 
-            
+            for (int i = 0; i < str.Count; i++)
+                Write_to_Console(str[i]);
         }
         public static  void Write_to_Console(string a)
         {
             Console.WriteLine(a);
-            Console.ReadLine();
+            Thread.Sleep(5000);
             Console.Clear();
         }
     }
